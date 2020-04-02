08265e5b355606865aa710c64c180e2b81e00219
saikongara <naga.hadoopdev@gmail.com>
2020-04-01 21:14:13 -0500

    Update recomm_take_home.py

--- recomm_take_home.py
+++ recomm_take_home.py
@@ -1,27 +1,11 @@
-# Databricks notebook source
-# MAGIC %md 
-# MAGIC I have made use of **Databricks Community Edition** to utilize `Spark 2.4.4` and `Python 3.7` to build the recommendation model. The data is loaded into DBFS (Databricks File System) and used as `/FileStore/tables/file_name`
-# MAGIC 
-# MAGIC Python Libraries used : `functions`,`window`,`requests`, `json` and `bigquery`.
-
-# COMMAND ----------
-
-# MAGIC %md ### Importing required modules
-
-# COMMAND ----------
-
 from pyspark.sql import functions as F
 from pyspark.sql import Window as W
 from pyspark.sql.types import StringType
 import uuid
 import json, requests
 
-# COMMAND ----------
 
-# MAGIC %md ### Loading the `Products` dataset provided
-
-# COMMAND ----------
-
+# Loading the `Products` dataset
 products = spark.read.option("delimiter", "\t").\
 option("inferSchema", "true").\
 csv("/FileStore/tables/products.txt").\
@@ -29,60 +13,42 @@ withColumnRenamed("_c0", "product_id").\
 withColumnRenamed("_c1", "mch_code").\
 withColumnRenamed("_c2", "product_name")
 
-# COMMAND ----------
 
-# MAGIC %md ### Loading the `MCH categories` dataset provided
-
-# COMMAND ----------
-
+# Loading the `MCH categories` dataset provided
 mch_categories = spark.read.option("delimiter", "\t").\
 option("inferSchema", "true").\
 option("header", "true").\
 csv("/FileStore/tables/mch_categories.tsv").\
 withColumnRenamed("code", "mch_code")
 
-# COMMAND ----------
 
-# MAGIC %md ### Loading the `Transactions` dataset provided
-
-# COMMAND ----------
-
+# Loading the `Transactions` dataset provided
 transactions = spark.read.\
 option("inferSchema", "true").\
 json("/FileStore/tables/transactions.txt")
 
-# COMMAND ----------
+
 
 uuidUdf= F.udf(lambda : str(uuid.uuid4()),StringType())
 spark.udf.register("uuidUdf", uuidUdf)
 
-# COMMAND ----------
 
-# MAGIC %md ##### Limiting the number of transaction from the dataset to `10000` as the compute is not sufficient for processing
 
-# COMMAND ----------
-
+# Limiting the number of transaction from the dataset to `10000` as the compute is not sufficient for processing
 exploded_trx = transactions.select("customer", "date", F.explode("itemList").alias("items"), "store").withColumn("trx_id", F.lit(uuidUdf())).select("customer", "date","items.item", "items.price", "items.quantity", "store", "trx_id").cache().limit(10000)
 
-# COMMAND ----------
 
 win = W.partitionBy(F.col("left.item")).orderBy(F.col("count").desc())
 
-# COMMAND ----------
+
 
 cross_explode = exploded_trx.alias("left").crossJoin(exploded_trx.alias("right")).filter((F.col("left.item") != F.col("right.item")) & (F.col("left.trx_id") != F.col("right.trx_id"))).select("left.item", "right.item").groupBy("left.item", "right.item").count().withColumn("ranking", F.rank().over(win)).filter(F.col("ranking") <= 5).orderBy("left.item").drop("ranking", "count").groupBy("left.item").agg(F.collect_list("right.item").alias("recommended_items")).cache()
 
-# COMMAND ----------
-
-# MAGIC %md ### Verifying the recommended products for Item IDs `20592676_EA` and `20801754003_C15`
-# MAGIC Not able to find transaction results for `20801754003_C15` This could be the result of limiting the number of Transactions to `10000`. Also, this could change the accuracy of results as all the transactions were not considered for calculating the recommendation, but the model is accurate in identifying the top 5 recommended products for given Item IDs.
-
-# COMMAND ----------
+# Verifying the recommended products for Item IDs `20592676_EA` and `20801754003_C15`
+# Not able to find transaction results for `20801754003_C15` This could be the result of limiting the number of Transactions to `10000`. Also, this could change the accuracy of results as all the transactions were not considered for calculating the recommendation, but the model is accurate in identifying the top 5 recommended products for given Item IDs.
 
 cross_explode = cross_explode.filter(F.col("left.item").isin(["20592676_EA","20801754003_C15"]))
 cross_explode.show()
-
-# COMMAND ----------
 
 def rearrange_items(data):
   data_map = json.loads(data)
@@ -90,7 +56,6 @@ def rearrange_items(data):
   result[data_map["item"]] = data_map["recommended_items"]
   return result
 
-# COMMAND ----------
 
 to_send = {}
 to_send["name"] = "Naga Sai"
@@ -102,11 +67,8 @@ for p in prods:
 
 print(to_send) 
 
-# COMMAND ----------
 
-# MAGIC %md ### What are the item ID and name of the top 5 co-purchased items for item with ID 20592676_EA? What about 20801754003_C15?
-
-# COMMAND ----------
+# What are the item ID and name of the top 5 co-purchased items for item with ID 20592676_EA? What about 20801754003_C15?
 
 try:
   recomm_itemID_for_20592676_EA = to_send['20592676_EA']
@@ -114,7 +76,6 @@ try:
 except KeyError as e:
   print("Transactions does not contain provided Item ID : {}".format(e))
 
-# COMMAND ----------
 
 try:
   top5_recomm_products_for_20592676_EA = products.filter(products["product_id"].isin(recomm_itemID_for_20592676_EA)).select("product_id","product_name")
@@ -130,18 +91,12 @@ except NameError as e:
   print("Not able to identify the Recommended products for 20801754003_C15")
   print("Error : {}".format(e))
 
-# COMMAND ----------
+# How would you deploy and serve the model?
+# 1. Assuming The data sources(3) would be designed as tables in database and this model connects to database using JDBC/ODBC connections. Once the testing of this model was successful, the code can be deployed onto Production (Python 3.7 and Spark 2.4.4) and triggers whenever the schedules are in place.
+# 2. Assuming the the data sources would be real-time, let us consider as kafka events, the code needs a bit more tweaks using Dstreams and StructuralStreams in Spark and the events gets processed real-time. In this scenario code needs to be deployed onto production upon successful completion of testing and triggered once to be executed contineously.
 
-# MAGIC %md ### How would you deploy and serve the model?
-# MAGIC 
-# MAGIC 1. Assuming The data sources(3) would be designed as tables in database and this model connects to database using JDBC/ODBC connections. Once the testing of this model was successful, the code can be deployed onto Production (Python 3.7 and Spark 2.4.4) and triggers whenever the schedules are in place.
-# MAGIC 2. Assuming the the data sources would be real-time, let us consider as kafka events, the code needs a bit more tweaks using Dstreams and StructuralStreams in Spark and the events gets processed real-time. In this scenario code needs to be deployed onto production upon successful completion of testing and triggered once to be executed contineously.
+# POST request submission to **Test Service**
 
-# COMMAND ----------
-
-# MAGIC %md ### POST request submission to **Test Service** : `https://ld-ds-take-home-test.appspot.com/submissions`
-
-# COMMAND ----------
 
 from google.cloud import bigquery
 from google.oauth2 import service_account
@@ -152,7 +107,6 @@ credentials = service_account.Credentials.from_service_account_file(key_path)
 
 client = bigquery.Client(credentials=credentials,project=credentials.project_id)
 
-# COMMAND ----------
 
 def fetch_response(to_send):
     api_url = "https://ld-ds-take-home-test.appspot.com/submissions"
@@ -163,12 +117,6 @@ def fetch_response(to_send):
   
 fetch_response(to_send)
 
-# COMMAND ----------
 
-# MAGIC %md 
-# MAGIC 
-# MAGIC   Response says Credentials are Invalid; Unable to extract the Credentials as required by the Test Servcie available in Google App Engine. However the approach used to extract the response and Credentials was accurate. The references provided in the .pdf for Google Cloud were used upto some extent for fetching Credential and the API calls were made in a different approach that the reference.
-
-# COMMAND ----------
 
 
